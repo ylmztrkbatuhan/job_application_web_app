2338134f09a18c243b48aa36c0cd49a2536990ae
oboy21 <oboy11b@gmail.com>
2024-08-10 10:08:57 +0300

    sending confirmation email

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "myapplication123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "app mail"
+app.config["MAIL_PASSWORD"] = "EMAİL PASSWORD"
+
 db = SQLAlchemy(app)
 
+mail = Mail(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -31,6 +39,17 @@ def index():
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank you for your submission, {first_name}" \
+                       f"Here are your data: {first_name}\n{last_name}\n{date}"\
+                       f"Thank you!"
+        message = Message(subject="New Form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
+        flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
